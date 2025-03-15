48d939472b4bb4e678e0a486d4bfc997dcb449aa
francescoaul <francesco.aul@outlook.com>
2025-03-15 18:35:40 -0400

    Added Temperature Converter files

---
+++ converter.js
@@ -0,0 +1,10 @@
+let fahrenheit = 100;
+let celsius = 0;
+let kelvin = 273.15
+
+celsius = ((fahrenheit - 32) * 5 ) /9;
+kelvin = celsius + 273.15;
+
+console.log('fahrenheit: ' + fahrenheit);
+console.log('celsius: ' + celsius);
+console.log('kelvin: ' + kelvin);
